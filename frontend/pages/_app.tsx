73e01db11d21456c5473fa328748ca398606e3a4
King Sway (Marcos Oranmiyan) <knucles0906@live.com>
2025-01-21 14:45:59 -0500

    Closed Version
    A versão atual não representa mais os últimos desenvolvimentos. As novas funcionalidades foram adicionadas em outro projeto, que será migrado para cá.

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,14 +1,19 @@
-import { ConfigProvider, theme } from 'antd';
+import { Provider } from "@/components/ui/provider"
 import type { AppProps } from 'next/app'
 import React from 'react';
 import "./_app.css";
 
 export default function MyApp({ Component, pageProps }: AppProps): React.JSX.Element {
 
-
   return (
-    <ConfigProvider theme={{ algorithm: theme.darkAlgorithm }}>
-      <Component {...pageProps} />
-    </ConfigProvider>
+    <html suppressHydrationWarning>
+      <body>
+        <Provider>
+          {/* {children} */}
+          <Component {...pageProps} />
+
+        </Provider>
+      </body>
+    </html>
   );
 } 
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,65 +1,69 @@
-import { INotifyApp } from '@/interfaces';
+// import { INotifyApp } from '@/interfaces';
 import { appConfigFetcher } from '@/services/requests';
-import { Breadcrumb, Layout, Menu, MenuProps, notification } from 'antd';
+// import { MenuProps, notification } from 'antd';
+// import { Breadcrumb, Layout, Menu, MenuProps, notification } from 'antd';
 import React from 'react';
 import useSWR from 'swr';
-type MenuItem = Required<MenuProps>['items'][number];
-import { FaSitemap } from "react-icons/fa";
-import { IoSettingsOutline } from "react-icons/io5";
+// import { FaSitemap } from "react-icons/fa";
+// import { IoSettingsOutline } from "react-icons/io5";
+import { Button } from '@chakra-ui/react';
 
-const { Header, Content, Footer, Sider } = Layout;
+// const { Header, Content, Footer, Sider } = Layout;
+// type MenuItem = Required<MenuProps>['items'][number];
 
 export default function Home(): React.JSX.Element {
 
-  const [api, contextHolder] = notification.useNotification();
+  // const [api, contextHolder] = notification.useNotification();
 
   const { data: appConfig, isLoading: isAppConfigLoading } = useSWR('/api/app_config', appConfigFetcher, { refreshInterval: 1000 });
 
   console.log("appConfig", appConfig);
+  console.log("isAppConfigLoading", isAppConfigLoading);
 
-  const notifyApp: INotifyApp = {
-    error: (message: string) => api.error({ closable: true, message }),
-    success: (message: string) => api.success({ closable: true, message }),
-    info: (message: string) => api.info({ closable: true, message }),
-  };
+  // const notifyApp: INotifyApp = {
+  //   error: (message: string) => api.error({ closable: true, message }),
+  //   success: (message: string) => api.success({ closable: true, message }),
+  //   info: (message: string) => api.info({ closable: true, message }),
+  // };
 
-  const [collapsed, setCollapsed] = React.useState(false);
+  // const [collapsed, setCollapsed] = React.useState(false);
 
-  const items: MenuItem[] = [
-    { key: "1", label: "Sites", icon: <FaSitemap /> },
-    { key: "2", label: "App Config", icon: <IoSettingsOutline /> },
-  ];
-
-  return (
-    <Layout style={{ minHeight: '100vh' }}>
-      {contextHolder}
-      <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
-        <div className="header-title">
-          <div className='title'>
-            {appConfig ? appConfig.title : "Lists App"}
-          </div>
-        </div>
-        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
-      </Sider>
-      <Layout>
-        <Header style={{ padding: 0 }} />
-        <Content style={{ margin: '0 16px' }}>
-          <Breadcrumb style={{ margin: '16px 0' }} items={[{ title: "Home", key: "Home" }]} />
-          <div className='content'>
-            <span>teste</span>
-          </div>
-        </Content>
-        <Footer style={{ textAlign: 'center' }}>
-          Ant Design ©{new Date().getFullYear()} Created by Ant UED
-        </Footer>
-      </Layout>
-    </Layout>
-  );
+  // const items: MenuItem[] = [
+  //   { key: "1", label: "Sites", icon: <FaSitemap /> },
+  //   { key: "2", label: "Settings", icon: <IoSettingsOutline /> },
+  // ];
 
   return (
     <div>
-      {contextHolder}
+      {/* {contextHolder} */}
       {/* <h1>{appConfig?.title}</h1> */}
+      <Button>Teste</Button>
     </div>
   );
+
+  // return (
+  //   <Layout style={{ minHeight: '100vh' }}>
+  //     {contextHolder}
+  //     <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
+  //       <div className="header-title">
+  //         <div className='title'>
+  //           {appConfig ? appConfig.title : "Lists App"}
+  //         </div>
+  //       </div>
+  //       <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
+  //     </Sider>
+  //     <Layout>
+  //       <Header style={{ padding: 0 }} />
+  //       <Content style={{ margin: '0 16px' }}>
+  //         <Breadcrumb style={{ margin: '16px 0' }} items={[{ title: "Home", key: "Home" }]} />
+  //         <div className='content'>
+  //           <span>teste</span>
+  //         </div>
+  //       </Content>
+  //       <Footer style={{ textAlign: 'center' }}>
+  //         Ant Design ©{new Date().getFullYear()} Created by Ant UED
+  //       </Footer>
+  //     </Layout>
+  //   </Layout>
+  // );
 }
